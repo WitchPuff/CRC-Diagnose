8dd11ec81477cd5aa36d3cd89f66f794115289a6
WitchPuff <78261579+WitchPuff@users.noreply.github.com>
2024-02-27 01:09:12 +0800

    Chinese

--- gui/frontend/src/pages/Profile/EditProfileForm.vue
+++ gui/frontend/src/pages/Profile/EditProfileForm.vue
@@ -1,8 +1,8 @@
 <!-- eslint-disable vue/no-mutating-props -->
 <template>
   <card :style="{ textAlign: 'center' }">
-    <h3 slot="header" class="title">AI-Powered CRC Diagnosis
-      <h4>via Image Upload</h4>
+    <h3 slot="header" class="title">AI辅助直结肠癌诊断
+      <!-- <h4>via Image Upload</h4> -->
     </h3>
     <div class="row">
           <div class="upload-box" @click="$refs.fileInput.click()" >
@@ -11,8 +11,9 @@
           </div>
     </div> 
 
-    <h3 :style="{ marginTop: '2%' }">Label: {{ prediction }}</h3>
-    <h4 :style="{ marginTop: '-1%' }">Model:
+    <h3 :style="{ marginTop: '2%' }">预测标签: {{ prediction }}</h3>
+    <h3 :style="{ marginTop: '-1%' }">AI诊断意见: {{ note }}</h3>
+    <h4 :style="{ marginTop: '-1%' }">模型选择:
       <select v-model="model" style="margin-left: 10px; height: 1.5em;">
         <option value="">--model--</option>
         <option value="resnet">ResNet</option>
@@ -30,7 +31,7 @@
         fill 
         @click="voice4model()"
       >
-        Voice Control
+        声音控制
       </base-button>
       <br>
       <span :style="{marginLeft: '1%', fontSize: '0.7em'}">{{ state }}</span>
@@ -52,7 +53,8 @@ export default {
       model: null,
       prediction: null,
       state: null,
-      message: null
+      message: null,
+      note: null
     };
   },
   methods: {
@@ -80,6 +82,48 @@ export default {
           // 这里处理服务器的响应
           this.message = null;
           this.prediction = response.data.result;
+          // 根据不同的 prediction 设置相应的 note 和完整名词
+          switch (this.prediction) {
+            case 'ADI':
+              this.note = '脂肪组织 - 正常脂肪组织，未见异常。';
+              this.prediction = 'Adipose';
+              break;
+            case 'BACK':
+              this.note = '背景组织 - 组织切片的基础结构，无异常发现。';
+              this.prediction = 'Background';
+              break;
+            case 'DEB':
+              this.note = '碎片 - 可能是由于组织制备过程中的破损，未显示任何特定病理变化。';
+              this.prediction = 'Debris';
+              break;
+            case 'LYM':
+              this.note = '淋巴细胞 - 表现出典型的淋巴细胞浸润，可能是炎症或免疫反应的结果。';
+              this.prediction = 'Lymphocytes';
+              break;
+            case 'MUC':
+              this.note = '黏液 - 存在黏液积聚，可能与某些疾病状态有关，例如炎症。';
+              this.prediction = 'Mucus';
+              break;
+            case 'MUS':
+              this.note = '平滑肌组织 - 正常的平滑肌组织，未见异常。';
+              this.prediction = 'Smooth Muscle';
+              break;
+            case 'NORM':
+              this.note = '正常结肠黏膜 - 组织呈正常结构，未见明显的病理变化。';
+              this.prediction = 'Normal Colon Mucosa';
+              break;
+            case 'STR':
+              this.note = '癌相关基质 - 存在与癌症关联的纤维组织，可能反映了肿瘤周围的反应性改变。';
+              this.prediction = 'Cancer-Associated Stroma';
+              break;
+            case 'TUM':
+              this.note = '结直肠腺癌上皮 - 存在结直肠腺癌的上皮组织，可能包括异型细胞和细胞增生。';
+              this.prediction = 'Colorectal Adenocarcinoma Epithelium';
+              break;
+            default:
+              // 默认情况，保持原始值
+              this.note = null;
+          }
           this.speakPrediction(this.prediction);
           console.log(response.data.result);
         })
@@ -112,10 +156,10 @@ export default {
       recognition.maxAlternatives = 1;
       recognition.start();
       console.log('started');
-      this.state = 'Recording started. Say 1/2 to choose the model for diagnosis.'
+      this.state = '开始录音，请说one/two来选择模型ResNet/ViT。'
       recognition.onresult = (event) => {
         // Get the spoken text
-        this.state = 'Transcribing...'
+        this.state = '正在转录...'
         const speechResult = event.results[0][0].transcript[0];
         console.log(speechResult);
         this.state = speechResult;
@@ -140,7 +184,7 @@ export default {
         recognition.stop();
         console.error('Error occurred in recognition:', event.error);
         console.log('stopped');
-        this.state = "Hearing nothing, recording stopped.";
+        this.state = "未识别到语音，录音暂停。";
       };
     } else {
       console.error('SpeechRecognition API is not supported in this browser.');
